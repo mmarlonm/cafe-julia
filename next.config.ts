c3c6fd92f56ca9590e2fa2d1c05f3ffe6dc260fd
c.marlgm <marlon93gm@gmail.com>
2026-07-30 10:19:46 -0600

    ajustes pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Requerido para GitHub Pages
-  images: {
-    unoptimized: true, // GitHub Pages no soporta la optimización de imágenes de Next.js
-  },
-  // Reemplaza 'cafe-julia' por el nombre EXACTO de tu repositorio en GitHub
+  output: 'export',
+  images: { unoptimized: true },
   basePath: process.env.NODE_ENV === 'production' ? '/cafe-julia' : '',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
